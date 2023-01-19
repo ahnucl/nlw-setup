5c3259a54529016f40fe55f1c4bb036f5ed4455b
Leonardo Cunha <leonardo.cunha@comunix.net.br>
2023-01-19 18:05:42 -0300

    Finaliza exibição de dias desabilitados

--- mobile/src/screens/Home.tsx
+++ mobile/src/screens/Home.tsx
@@ -1,4 +1,4 @@
-import { View, Text } from 'react-native'
+import { View, Text, ScrollView } from 'react-native'
 
 import { HabitDay, DAY_SIZE } from '../components/HabitDay'
 import { Header } from '../components/Header'
@@ -6,8 +6,9 @@ import { Header } from '../components/Header'
 import { generateDatesFromYearBeginning } from '../utils/generate-dates-from-year-beginning'
 
 const weekDays = ['D', 'S', 'T', 'Q', 'Q', 'S', 'S']
-
 const datesFromYearStart = generateDatesFromYearBeginning()
+const minimumSummaryDatesSizes = 18 * 5
+const amountOfDaysToFill = minimumSummaryDatesSizes - datesFromYearStart.length
 
 export function Home() {
   return (
@@ -26,11 +27,25 @@ export function Home() {
         ))}
       </View>
 
-      <View className="flex-row flex-wrap">
-        {datesFromYearStart.map((date) => (
-          <HabitDay key={date.toISOString()} />
-        ))}
-      </View>
+      <ScrollView
+        showsVerticalScrollIndicator={false}
+        contentContainerStyle={{ paddingBottom: 100 }}
+      >
+        <View className="flex-row flex-wrap">
+          {datesFromYearStart.map((date) => (
+            <HabitDay key={date.toISOString()} />
+          ))}
+
+          {amountOfDaysToFill > 0 &&
+            Array.from({ length: amountOfDaysToFill }).map((_, i) => (
+              <View
+                key={i}
+                className="bg-zinc-900 rounded-lg border-2 m-1 border-zinc-800 opacity-40"
+                style={{ width: DAY_SIZE, height: DAY_SIZE }}
+              />
+            ))}
+        </View>
+      </ScrollView>
     </View>
   )
 }
