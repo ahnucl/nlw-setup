e76dfa6b898e1b29392597026f5b0474f98fbfe2
Leonardo Cunha <leonardo.cunha@comunix.net.br>
2023-01-20 16:09:08 -0300

    Tipa navigation e cria botão de voltar

---
+++ mobile/src/@types/navigation.d.ts
@@ -0,0 +1,12 @@
+/* eslint-disable no-unused-vars */
+export declare global {
+  namespace ReactNavigation {
+    interface RootParamList {
+      home: undefined
+      new: undefined
+      habit: {
+        date: string
+      }
+    }
+  }
+}
