441ae356d1885edfa32a6128303c4739025af12d
Leonardo Cunha <lalmcunha@gmail.com>
2023-02-02 15:30:26 -0300

    Atualiza hábitos com o backend

--- mobile/src/screens/Home.tsx
+++ mobile/src/screens/Home.tsx
@@ -1,12 +1,12 @@
 import { View, Text, ScrollView, Alert } from 'react-native'
-import { useNavigation } from '@react-navigation/native'
+import { useNavigation, useFocusEffect } from '@react-navigation/native'
 
 import { api } from '../lib/axios'
 import { HabitDay, DAY_SIZE } from '../components/HabitDay'
 import { Header } from '../components/Header'
 
 import { generateDatesFromYearBeginning } from '../utils/generate-dates-from-year-beginning'
-import { useEffect, useState } from 'react'
+import { useCallback, useState } from 'react'
 import { Loading } from '../components/Loading'
 import dayjs from 'dayjs'
 
@@ -41,9 +41,11 @@ export function Home() {
     }
   }
 
-  useEffect(() => {
-    fetchData()
-  }, [])
+  useFocusEffect(
+    useCallback(() => {
+      fetchData()
+    }, []),
+  )
 
   if (loading) {
     return <Loading />
