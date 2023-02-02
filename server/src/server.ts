b8024856c1356f1528267ffab9e6d5e022a01f10
Leonardo Cunha <lalmcunha@gmail.com>
2023-02-02 12:50:11 -0300

    Publica server dev e unifica nome do script

--- server/src/server.ts
+++ server/src/server.ts
@@ -2,15 +2,20 @@ import cors from '@fastify/cors'
 import Fastify from 'fastify'
 import { appRoutes } from './route'
 
+const listenConfig = process.env.PUBLIC_HOST
+  ? {
+      host: '0.0.0.0',
+      port: 3333,
+    }
+  : {
+      port: 3333,
+    }
+
 const app = Fastify()
 
 app.register(cors) // Qualquer aplicação pode consumir esse back-end
 app.register(appRoutes)
 
-app
-  .listen({
-    port: 3333,
-  })
-  .then(() => {
-    console.log('HTTP Server running!')
-  })
+app.listen(listenConfig).then(() => {
+  console.log('HTTP Server running!')
+})
